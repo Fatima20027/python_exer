41f1cd8b37dbb02c2c22d65195a0eae0582298e8
Fatima20027 <132764022+Fatima20027@users.noreply.github.com>
2024-03-07 16:16:00 +0100

    Add files via upload
    car file added

---
+++ car_fun.py
@@ -0,0 +1,26 @@
+def My_car():
+    car = input("> ")
+    if "start" in car :
+        print("car starts moving")
+        
+    elif car == "stop" :
+        print("car has stopped moving")
+     
+    elif car == "help" :
+        print("-start: moving the car", end = "\n\n")
+        print("-stop: stopping the car", end = "\n\n")
+        print("-quit: quitting the car", end = "\n\n")
+    elif car == "quit" :
+        print("quitting the car")
+        return
+    
+    else :
+        print("this command not found")
+        print("type --help for more")
+    My_car()
+My_car()
+
+   
+
+
+
